88f836062aa6add3696ce233669cdc543261f6cd
Arkraj Kundu <83090891+arkrajkundu@users.noreply.github.com>
2024-09-14 23:28:06 +0530

    Update transcriptRoutes.js

--- backend/routes/transcriptRoutes.js
+++ backend/routes/transcriptRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getTranscript, updateTranscript } = require('../controllers/transcriptController');
 
-// Route to get the transcript data
 router.get('/transcript', getTranscript);
-
-// Route to update a transcript word
 router.post('/transcript/update', updateTranscript);
 
 module.exports = router;
